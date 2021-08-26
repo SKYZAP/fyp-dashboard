3e66526be982753da26e41c2fb2670d1a93c54c5
Skyzap <izzat.jehan@gmail.com>
2021-08-27 02:21:37 +0800

    Updated next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+const withAntdLess = require("next-plugin-antd-less");
+
+module.exports = withAntdLess({
+  modifyVars: { "@primary-color": "#9fd356" },
+  lessVarsFilePathAppendToEndOfContent: false,
+  cssLoaderOptions: {},
+  webpack5: true,
+  webpack: (config) => {
+    return config;
+  },
+});
