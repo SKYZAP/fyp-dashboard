fd4d650b76b122494d5ee5fc0c6da9dd23f7d101
Skyzap <izzat.jehan@gmail.com>
2021-09-22 01:03:56 +0800

    Updated hello api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import dbConn from "../../src/utils";
+
+const dbClient = dbConn();
 
 type Data = {
-  name: string;
+  ip: string;
 };
 
-const handler = (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  console.log(req.query);
-  res.status(200).json({ name: `${req.query.name}` });
+const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  await dbClient.connect();
+  const result = await dbClient.query(
+    `SELECT * FROM jetracer WHERE "ipAddress" = $1`,
+    [req.query.ip],
+  );
+  console.log(req.query.ip);
+  await dbClient.clean();
+  // console.log(req.query);
+  // res.status(200).json({ name: `${req.query.name}` });
+  res.status(200).json(result.rows[0]);
 };
 
 export default handler;
