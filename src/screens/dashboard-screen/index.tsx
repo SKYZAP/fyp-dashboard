f178968d119d4e852aa66a3ce535d5d64f1d311c
Skyzap <izzat.jehan@gmail.com>
2021-09-28 00:57:01 +0800

    Added data and message display

--- src/screens/dashboard-screen/index.tsx
+++ src/screens/dashboard-screen/index.tsx
@@ -1,8 +1,67 @@
-import { Layout, Menu, Breadcrumb, Image, Skeleton } from "antd";
+import { Layout, Menu, Breadcrumb, Image, Skeleton, Table, Button } from "antd";
 import { useState } from "react";
+import { MessageList } from "react-chat-elements";
 require("antd/dist/antd.less");
+require("react-chat-elements/dist/main.css");
 
 const { Header, Content, Footer } = Layout;
+
+const msg = {
+  position: "right",
+  type: "text",
+  text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+  date: new Date(),
+};
+
+const data = [
+  {
+    key: "1",
+    image:
+      "https://cdn.vox-cdn.com/thumbor/8tLchaDMIEDNzUD3mYQ7v1ZQL84=/0x0:2012x1341/920x613/filters:focal(0x0:2012x1341):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg",
+    date: new Date().toLocaleString(),
+  },
+  {
+    key: "2",
+    image:
+      "https://cdn.vox-cdn.com/thumbor/8tLchaDMIEDNzUD3mYQ7v1ZQL84=/0x0:2012x1341/920x613/filters:focal(0x0:2012x1341):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg",
+    date: new Date().toLocaleString(),
+  },
+  {
+    key: "3",
+    image:
+      "https://cdn.vox-cdn.com/thumbor/8tLchaDMIEDNzUD3mYQ7v1ZQL84=/0x0:2012x1341/920x613/filters:focal(0x0:2012x1341):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg",
+    date: new Date().toLocaleString(),
+  },
+  {
+    key: "4",
+    image:
+      "https://cdn.vox-cdn.com/thumbor/8tLchaDMIEDNzUD3mYQ7v1ZQL84=/0x0:2012x1341/920x613/filters:focal(0x0:2012x1341):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg",
+    date: new Date().toLocaleString(),
+  },
+  {
+    key: "5",
+    image:
+      "https://cdn.vox-cdn.com/thumbor/8tLchaDMIEDNzUD3mYQ7v1ZQL84=/0x0:2012x1341/920x613/filters:focal(0x0:2012x1341):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg",
+    date: new Date().toLocaleString(),
+  },
+];
+
+const columns = [
+  {
+    title: "Image",
+    dataIndex: "image",
+    key: "image",
+    // eslint-disable-next-line react/display-name
+    render: (text) => (
+      <Image style={{ maxHeight: "150px" }} alt="" src={text} />
+    ),
+  },
+  {
+    title: "Date",
+    dataIndex: "date",
+    key: "date",
+  },
+];
 
 const ContentRender = (key) => {
   const [loading, setLoading] = useState(true);
@@ -18,27 +77,52 @@ const ContentRender = (key) => {
 
   if (key == "LivePreview") {
     return (
-      <div className="site-layout-content" style={{ display: "flex" }}>
-        {loading ? (
-          <Skeleton.Image style={{ width: "75em", height: "100%" }} />
-        ) : (
-          <Image width="75%" alt="CAMERA STREAM" src={result} />
-        )}
-        <div
-          className="streamChat"
-          style={{
-            border: "solid 1px black",
-            margin: "auto",
-            width: "20%",
-            height: "50em",
-          }}
-        >
-          Text
+      <>
+        <Breadcrumb style={{ margin: "16px 0" }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>Camera</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className="site-layout-content" style={{ display: "flex" }}>
+          {loading ? (
+            <Skeleton.Image style={{ width: "75em", height: "100%" }} />
+          ) : (
+            <Image width="75%" alt="CAMERA STREAM" src={result} />
+          )}
+          <MessageList
+            style={{ borderStyle: "solid" }}
+            className="message-list"
+            lockable={true}
+            toBottomHeight={"100%"}
+            dataSource={[msg]}
+          />
         </div>
-      </div>
+      </>
     );
   } else if (key == "Datasets") {
-    return <div className="site-layout-content">DATA</div>;
+    return (
+      <>
+        <Breadcrumb style={{ margin: "16px 0" }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>Data</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className="site-layout-content">
+          <div style={{ width: "100%" }}>
+            <Button
+              style={{ position: "relative", float: "right", margin: "1em" }}
+              size="large"
+            >
+              Refresh
+            </Button>
+          </div>
+          <Table
+            pagination={{ pageSize: 3 }}
+            style={{ maxHeight: "70vh" }}
+            dataSource={data}
+            columns={columns}
+          />
+        </div>
+      </>
+    );
   }
 };
 
@@ -75,10 +159,6 @@ const DashboardScreen = () => {
         </Menu>
       </Header>
       <Content style={{ padding: "0 50px" }}>
-        <Breadcrumb style={{ margin: "16px 0" }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>Camera</Breadcrumb.Item>
-        </Breadcrumb>
         {ContentRender(selectedMenuItem)}
       </Content>
       <Footer style={{ textAlign: "center" }}>
