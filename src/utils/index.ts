a29169740889a48e2e6a0b306938494ad2abbd34
Skyzap <izzat.jehan@gmail.com>
2021-10-11 03:01:15 +0800

    Added google client util function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,8 @@
 const ServerlessClient = require("serverless-postgres");
+import fs from "fs";
+const { google } = require("googleapis");
 
-const dbConn = () => {
+export const dbConn = () => {
   const client = new ServerlessClient({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
@@ -17,4 +19,45 @@ const dbConn = () => {
   return client;
 };
 
-export default dbConn;
+export const googleClient = () => {
+  const fs = require("fs");
+  const path = require("path");
+  const readline = require("readline");
+  const { google } = require("googleapis");
+  const { authenticate } = require("@google-cloud/local-auth");
+
+  const drive = google.drive("v3");
+
+  async function runSample(fileName) {
+    // Obtain user credentials to use for the request
+    const auth = await authenticate({
+      keyfilePath: path.join(__dirname, "./token.json"),
+      scopes: "https://www.googleapis.com/auth/drive.file",
+    });
+    google.options({ auth });
+
+    const fileSize = fs.statSync(fileName).size;
+    const res = await drive.files.create(
+      {
+        requestBody: {
+          // a requestBody element is required if you want to use multipart
+        },
+        media: {
+          body: fs.createReadStream(fileName),
+        },
+      },
+      {
+        // Use the `onUploadProgress` event from Axios to track the
+        // number of bytes uploaded to this point.
+        onUploadProgress: (evt) => {
+          const progress = (evt.bytesRead / fileSize) * 100;
+          readline.clearLine(process.stdout, 0);
+          readline.cursorTo(process.stdout, 0);
+          process.stdout.write(`${Math.round(progress)}% complete`);
+        },
+      },
+    );
+    console.log(res.data);
+    return res.data;
+  }
+};
