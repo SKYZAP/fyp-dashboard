72a8ca0540c75b5adec81eae2d2f2a81e308bd01
Skyzap <izzat.jehan@gmail.com>
2021-10-11 03:00:59 +0800

    Fixed imports

--- pages/api/getJetracer.ts
+++ pages/api/getJetracer.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import dbConn from "../../src/utils";
+import { dbConn } from "../../src/utils";
 
 const dbClient = dbConn();
 
