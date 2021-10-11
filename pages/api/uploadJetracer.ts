dfed9162817c0581b20aa594784dd53963ae70f7
Skyzap <izzat.jehan@gmail.com>
2021-10-12 02:33:30 +0800

    Added request handling to uploadJetracer

--- pages/api/uploadJetracer.ts
+++ pages/api/uploadJetracer.ts
@@ -3,9 +3,13 @@ import type { NextApiRequest, NextApiResponse } from "next";
 type Data = {
   ip?: string;
   mac?: string;
+  file?: any;
   error?: string;
 };
 
-const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {};
+const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  console.log("IMAGE: ", req);
+  res.status(200).json({ file: `` });
+};
 
 export default handler;
